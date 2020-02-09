d8324bd65543654b9170d799415fd33137af7b2c
Matt <mattlaws1982@gmail.com>
2020-02-09 17:17:40 +0000

    Jquery plugin working, min-width media query added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // HANDLE IMAGE FILTERING
 const search = document.querySelector('#mySearch');
-const thumbnail = document.querySelectorAll('thumbnail');
+const thumbnail = document.querySelectorAll('.item a');
 
 const handleSearch = event => {
     const searchTerm = event.target.value.toLowerCase();
@@ -8,7 +8,7 @@ const handleSearch = event => {
     thumbnail.forEach(imageSearch => {
         const text = imageSearch.getAttribute('data-title').toLowerCase();
         const box = imageSearch.parentElement;
-
+        
         if (text.indexOf(searchTerm) > -1) {
             box.style.display = "block";
         } else {
