f6976206d431f055114fc1792e1310c68e30335f
Matt <mattlaws1982@gmail.com>
2020-02-09 16:56:21 +0000

    JS added for the search & filter, Jquery plugin underway

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// HANDLE IMAGE FILTERING
+const search = document.querySelector('#mySearch');
+const thumbnail = document.querySelectorAll('.item a');
+
+const handleSearch = event => {
+    const searchTerm = event.target.value.toLowerCase();
+
+    .item.forEach(imageSearch => {
+        const text = imageSearch.getAttribute('data-title').toLowerCase();
+        const box = imageSearch.parentElement;
+
+        if (text.indexOf(searchTerm) > -1) {
+            box.style.display = "block";
+        } else {
+            box.style.display = "none";
+        }
+    });
+}
+
+search.addEventListener('keyup', handleSearch);
